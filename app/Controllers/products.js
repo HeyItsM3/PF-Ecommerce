927b09de2418abe9c391331985ad769692f25ee8
Frank <franco_cli_cli@hotmail.com>
2022-05-14 14:20:57 -0300

    validacion del post

--- app/Controllers/products.js
+++ app/Controllers/products.js
@@ -3,42 +3,13 @@ const { cloudinary } = require('../utils/utils')
 const ProductModel = require('../Models/products')
 const streamifier = require('streamifier')
 const multer = require('multer')
-
-// GET All Products // by name
-
-// multer.app.use(multer().single('image')) // Procesa o ve si se envia una imagen
-// // Single examina el campo (form, etc) por donde ingresa la imagen pueder ser array para multiples img
-// // En este caso ingresa por el input de tipo image
-
 const storage = multer.memoryStorage()
 const configMulter = multer({ storage }).single('image')
 
-// const uploadImage = async (req, res, next) => {
-//   if (!req.file) {
-//     res.satus(400).json({ msg: 'You have to upload a file' })
-//   }
-//   try {
-//     const streamUpload = (req) => {
-//       return new Promise((resolve, reject) => {
-//         const stream = cloudinary.uploader.upload_stream((error, result) => {
-//           if (result) {
-//             resolve(result)
-//           } else {
-//             reject(error)
-//           }
-//         })
-//         streamifier.createReadStream(req.file.buffer).pipe(stream)
-//       })
-//     }
-//     const result = await streamUpload(req)
+// // Single examina el campo (form, etc) por donde ingresa la imagen pueder ser array para multiples img
+// // En este caso ingresa por el input de tipo image
 
-//     console.log(result)
-//     res.status(200).json(result.url)
-//   } catch (err) {
-//     res.status(500).json({ msg: 'Filed to upload the image:' + err })
-//   }
-// }
-
+// GET Products
 const getAllProducts = async (req, res) => {
   const {
     query: { name },
@@ -57,9 +28,10 @@ const getAllProducts = async (req, res) => {
       res.status(200).json({ message: 'Successful request', product })
     }
   } catch (err) {
-    res
-      .status(500)
-      .json({ msg: 'Filed getAllProducts product controller' + err })
+    res.status(500).json({
+      msg: 'Filed getAllProducts product controller' + err,
+      error: true,
+    })
   }
 }
 
@@ -72,14 +44,18 @@ const getProductDetail = async (req, res) => {
   try {
     const product = await ProductModel.findById(id)
     product
-      ? res.status(200).json({ message: 'Successful request', product })
+      ? res
+          .status(200)
+          .json({ message: 'Successful request', product, error: false })
       : res.status(404).json({
-          message: `Error Request, the product with the id:${id} was not found `,
+          message: `Error Request, the product with the id:${id} was not found, `,
+          error: true,
         })
   } catch (err) {
-    res
-      .status(500)
-      .json({ msg: 'Filed getProductDetail product controller' + err })
+    res.status(500).json({
+      msg: 'Filed getProductDetail product controller' + err,
+      error: true,
+    })
   }
 }
 
@@ -99,51 +75,77 @@ const postProduct = async (req, res) => {
       offer,
       dimensions,
       other,
+      category,
     },
   } = req
 
   try {
-    // if (!file) {
-    //   res.satus(400).json({ msg: 'You have to upload a file' })
-    // }
-    const streamUpload = (req) => {
-      return new Promise((resolve, reject) => {
-        const stream = cloudinary.uploader.upload_stream((error, result) => {
-          if (result) {
-            resolve(result)
-          } else {
-            reject(error)
-          }
-        })
-        streamifier.createReadStream(req.file.buffer).pipe(stream)
-      })
-    }
-    const { url } = await streamUpload(req)
-    console.log(url)
+    if (
+      !name &&
+      !image &&
+      !brand &&
+      !description &&
+      !price &&
+      !amount &&
+      !condition &&
+      !model &&
+      !offer &&
+      !dimensions &&
+      !category
+      // eslint-disable-next-line no-empty
+    ) {
+      res
+        .status(404)
+        .json({ message: 'Error, a required data was not found', error: true })
+    } else {
+      if (!req.file) {
+        res.satus(400).json({ msg: 'You have to upload a file', error: true })
+      } else {
+        const streamUpload = (req) => {
+          return new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream(
+              (error, result) => {
+                if (result) {
+                  resolve(result)
+                } else {
+                  reject(error)
+                }
+              }
+            )
+            streamifier.createReadStream(req.file.buffer).pipe(stream)
+          })
+        }
+        const { url } = await streamUpload(req)
+        console.log(url)
 
-    const newProduct = {
-      name,
-      brand,
-      description,
-      price,
-      amount,
-      condition,
-      model,
-      offer,
-      dimensions,
-      other,
-      image: url,
-    }
+        const newProduct = {
+          name,
+          brand,
+          description,
+          price,
+          amount,
+          category,
+          condition,
+          model,
+          offer,
+          dimensions,
+          other,
+          image: url,
+        }
 
-    const product = await ProductModel.create(newProduct)
-    product
-      ? res.status(200).json({
-          message: 'Successful request',
-          product,
-        })
-      : res.status(404).json({ message: 'Error' })
+        const product = await ProductModel.create(newProduct)
+        product
+          ? res.status(202).json({
+              message: 'Successful request',
+              product,
+            })
+          : res.status(404).json({ message: 'Error' })
+      }
+    }
   } catch (err) {
-    res.status(500).json({ msg: 'Filed post product controller' + err })
+    res
+      .status(500)
+      .json({ msg: 'Filed post product controller' + err, error: true })
   }
 }
 
@@ -164,7 +166,9 @@ const deleteProduct = async (req, res) => {
           message: `Error Request, the product with the id:${id} was not found `,
         })
   } catch (err) {
-    res.status(500).json({ msg: 'Filed deleteProduct controller' + err })
+    res
+      .status(500)
+      .json({ msg: 'Filed deleteProduct controller' + err, error: true })
   }
 }
 
@@ -190,7 +194,9 @@ const upDateProduct = async (req, res) => {
     Unable to update the product please check if the id is correct`,
         })
   } catch (err) {
-    res.status(500).json({ msg: 'Filed upDateProduct controller' + err })
+    res
+      .status(500)
+      .json({ msg: 'Filed upDateProduct controller' + err, error: true })
   }
 }
 
--- app/Routes/products.js
+++ app/Routes/products.js
@@ -1,20 +1,18 @@
 const { Router } = require('express')
-
+const { validateCreateProduct } = require('../validators/products')
 const {
   getAllProducts,
   getProductDetail,
   postProduct,
   upDateProduct,
   deleteProduct,
-  uploadImage,
   configMulter,
 } = require('../Controllers/products')
 const router = Router()
 
 router.get('/', getAllProducts)
 router.get('/detail/:id', getProductDetail)
-router.post('/post', configMulter, postProduct)
-// router.post('/upload', configMulter, uploadImage)
+router.post('/post', configMulter, validateCreateProduct, postProduct)
 router.delete('/delete/:id', deleteProduct)
 router.put('/update/:id', upDateProduct)
 
