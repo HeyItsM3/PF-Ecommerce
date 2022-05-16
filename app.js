92c024343936f23ae5a21074bb07a812dde47ba3
Aldo <aldoyedro181@gmail.com>
2022-05-15 21:59:33 -0300

    Cors

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(express.urlencoded({ limit: '50bm', extended: true }))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
 //* ROUTES
 app.use('/api', router)
