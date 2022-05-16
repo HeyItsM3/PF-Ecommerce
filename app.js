ed0ebe39246138ab95ec89f068632f615a02ca51
Aldo <aldoyedro181@gmail.com>
2022-05-15 22:11:48 -0300

    fix

--- app.js
+++ app.js
@@ -16,18 +16,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(express.urlencoded({ limit: '50bm', extended: true }))
 app.use(
-  cors((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, DELETE'
-    )
-    next()
+  cors({
+    origin: 'http://localhost:3000',
   })
 )
 
