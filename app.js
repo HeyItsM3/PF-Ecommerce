a9ccfba8577d27dc4e1f5dcd8423ba8ec6171141
Aldo <aldoyedro181@gmail.com>
2022-05-15 22:06:42 -0300

    cors

--- app.js
+++ app.js
@@ -11,21 +11,25 @@ const { dbConnect } = require('./config/mongo')
 const router = require('./app/Routes')
 
 //* MIDDLEWARE
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(express.urlencoded({ limit: '50bm', extended: true }))
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  next()
-})
+app.use(
+  cors((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', 'true')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, DELETE'
+    )
+    next()
+  })
+)
 
 //* ROUTES
 app.use('/api', router)
