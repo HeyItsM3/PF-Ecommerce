cb146a7a1779d3dc559415a5f3bb12bb824fc9ea
Aldo <aldoyedro181@gmail.com>
2022-05-27 12:53:08 -0300

    Orders

--- app/Controllers/order.js
+++ app/Controllers/order.js
@@ -13,7 +13,7 @@ const getAllOrders = async (req, res, next) => {
 
 // POST A NEW ORDER
 
-const postOrder = async (req, res) => {
+const postOrder = async (req, res, next) => {
   const {
     orderProducts,
     deliveryAddress,
@@ -25,18 +25,22 @@ const postOrder = async (req, res) => {
   if (orderProducts && orderProducts.length === 0) {
     return res.status(400).json({ msg: 'You need to add orderProducts' })
   } else {
-    const order = new OrderModel({
-      orderProducts,
-      deliveryAddress,
-      paymentMethod,
-      itemsPrice,
-      shippingPrice,
-      totalPrice,
-      // user: req.data.user._id,
-    })
-    const newOrder = await order.save()
+    try {
+      const order = new OrderModel({
+        orderProducts,
+        deliveryAddress,
+        paymentMethod,
+        itemsPrice,
+        shippingPrice,
+        totalPrice,
+        user: req.data.user._id,
+      })
+      const newOrder = await order.save()
 
-    res.status(201).json({ msg: 'Order created successfully', newOrder })
+      res.status(201).json({ msg: 'Order created successfully', newOrder })
+    } catch (err) {
+      next(new Error('Error in postOrder' + err))
+    }
   }
 }
 
@@ -53,7 +57,8 @@ const getOrderDetail = async (req, res) => {
 
 // SET DELIVERY ORDER
 
-const setOrderDelivery = async (req, res) => {
+const setOrderDelivery = async (req, res, next) => {
+  if (!req.params.id) next(new Error('You need to provide an id'))
   const order = await OrderModel.findById(req.params.id)
   if (order) {
     order.isDelivered = true
@@ -61,27 +66,31 @@ const setOrderDelivery = async (req, res) => {
     await order.save()
     res.status(200).json({ message: 'Order Delivered' })
   } else {
-    res.status(404).json({ message: 'Order Not Found' })
+    next(new Error('Order Not Found'))
   }
 }
 
 // DELETE ORDER
 
-const deleteOrder = async (req, res) => {
+const deleteOrder = async (req, res, next) => {
   const order = await OrderModel.findById(req.params.id)
   if (order) {
     await order.remove()
     res.status(200).json({ message: 'Order Deleted' })
   } else {
-    res.status(404).json({ message: 'Order Not Found' })
+    next(new Error('Order Not Found'))
   }
 }
 
 // GET ORDERS BY USER ID
 
-const getUserOrders = async (req, res) => {
-  const userOrder = await OrderModel.find({ user: req.data.user._id })
-  res.status(200).json(userOrder)
+const getUserOrders = async (req, res, next) => {
+  try {
+    const userOrder = await OrderModel.find({ user: req.data.user._id })
+    res.status(200).json(userOrder)
+  } catch (err) {
+    next(new Error('User Order Not Found' + err))
+  }
 }
 
 // PAY ORDER
