c6658289bf9b1527b343b97459a6e17d5a25449d
Aldo <aldoyedro181@gmail.com>
2022-05-29 19:38:55 -0300

    Categories

--- app/Controllers/products.js
+++ app/Controllers/products.js
@@ -1,4 +1,4 @@
-const { streamUpload } = require('../utils/utils')
+// const { streamUpload } = require('../utils/utils')
 const ProductModel = require('../Models/products')
 const _ = require('lodash')
 
@@ -91,23 +91,22 @@ const postProduct = async (req, res, next) => {
       model,
       screenSize,
       internalMemory,
+      categories,
       brand,
     },
   } = req
 
   try {
-    if (!req.files[0]) {
-      return res.status(400).json({ msg: 'You have to upload an image' })
-    }
-    const urls = []
-    const files = req.files
-    for (const file of files) {
-      const { buffer } = file
-      const newPath = await streamUpload(buffer)
-      urls.push(newPath)
-    }
-
-    // const categoriesArr = req.body.map((categories) => categories.categories)
+    // if (!req.files[0]) {
+    //   return res.status(400).json({ msg: 'You have to upload an image' })
+    // }
+    // const urls = []
+    // const files = req.files
+    // for (const file of files) {
+    //   const { buffer } = file
+    //   const newPath = await streamUpload(buffer)
+    //   urls.push(newPath)
+    // }
 
     const newProduct = {
       name,
@@ -115,12 +114,12 @@ const postProduct = async (req, res, next) => {
       description,
       price,
       amountInStock,
-      // categories: categoriesArr,
+      categories,
       condition,
       model,
       screenSize,
       internalMemory,
-      image: urls,
+      // image: urls,
     }
 
     const product = await ProductModel.create(newProduct)
