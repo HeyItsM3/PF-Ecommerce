14f1701274cde1d256d52464467f3efba29a12a7
Aldo <aldoyedro181@gmail.com>
2022-05-30 06:10:23 -0300

    Wishlist working

--- app/Routes/wishlist.routes.js
+++ app/Routes/wishlist.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require('express')
-const { insertToWishlist, findWishlist } = require('../Controllers/wishlist')
-const { isAuth } = require('../middleware/authentication.js')
+const {
+  insertToWishlist,
+  findWishlist,
+  getAllWishlist,
+  deleteFromWishlist,
+} = require('../Controllers/wishlist')
+const { isAuth, isAdmin } = require('../middleware/authentication.js')
 const router = Router()
 
-router.get('/add', isAuth, insertToWishlist)
-router.get('/user', isAuth, findWishlist)
+router.post('/add/:productId', isAuth, insertToWishlist)
+router.get('/user/', isAuth, findWishlist)
+router.delete('/delete/:productId', isAuth, deleteFromWishlist)
+router.get('/all', isAuth, isAdmin, getAllWishlist)
 
 module.exports = router
